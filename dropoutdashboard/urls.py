815fafda4ec5467a511450a68fd87003a09e68f7
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2022-04-30 22:19:27 -0300

    A basic MVC

--- dropoutdashboard/urls.py
+++ dropoutdashboard/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path('', views.index, name='index'),
+
+    path('admin/', admin.site.urls),
 ]
--- dropoutdashboard/views.py
+++ dropoutdashboard/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def student_show(request):
-    x = []
-    for i in range(10):
-        x.append(i)
-    return HttpResponse("<h1>DataFlair Django Tutorials</h1>The Digits are {0}".format(x))
+from .utils2 import aux 
+
+def index(request):
+    dados = aux.load()
+    return render(request, 'dropoutdashboard/index.html', {'dados': dados })
