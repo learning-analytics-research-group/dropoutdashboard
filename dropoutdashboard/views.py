aab6906c5d52618075c51077e48e1086f58175cc
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2022-04-30 21:38:37 -0300

    moving a level up

---
+++ dropoutdashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def student_show(request):
+    x = []
+    for i in range(10):
+        x.append(i)
+    return HttpResponse("<h1>DataFlair Django Tutorials</h1>The Digits are {0}".format(x))
